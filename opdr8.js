efaa3ac0689a9a5100c4c4785b896085cc89a783
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-15 17:01:39 +0100

    Add files via upload

--- opdr8.js
+++ opdr8.js
@@ -1,6 +1,31 @@
-const multi = function (4) {
-    console.log(multi(* 2)); {
-        return
-    }
-};
+/*
+function doSquareCalculation(number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+}
+*/
 
+/*
+const doSquareCalculation = function (number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+*/
+
+/*
+const doSquareCalculation = function(number1, number2) {
+    const sum = number1 * number1 + number2 * number2;
+    return sum * sum;
+};
+*/
+
+const doSquareCalculation = (number1, number2) => {
+    const sum = number1 * number1 + number2 * number2;
+    return sum * sum;
+};
